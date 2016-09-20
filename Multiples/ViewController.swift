fb0f8919eed69dbb3c5d682dda0d8ac3bacda007
Peter Zaporowski <peter@Peters-Mac.local>
2016-09-20 20:24:13 +0200

    Some warnings with AutoLayout, overall happy

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -4,22 +4,79 @@
 //
 //  Created by Peter Zaporowski on 20.09.2016.
 //  Copyright © 2016 Peter Zaporowski. All rights reserved.
-//
+//  made using Mark Price's example from Udemy iOS 9 course
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //IBOutlets
+    @IBOutlet weak var whatMultipleTxt: UITextField!
+    @IBOutlet weak var sumLbl: UILabel!
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    //Properties
+    var multiple = 0
+    var runningSum = 0
+    let MAX_SUM = 100
+    
+    
+    @IBAction func onAddBtnTapped(sender: AnyObject) {
+        //Since we never change the newSum value make it a constant
+        let newSum = runningSum + multiple
+        updateLbl(runningSum, mul: multiple, newSum: newSum)
+        runningSum += multiple
+        
+        if isGameOver() {
+            restartGame()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onPlayBtnTapped(sender: AnyObject) {
+        
+        if whatMultipleTxt.text != nil && whatMultipleTxt.text != "" {
+            
+            whatMultipleTxt.hidden = true
+            logoImg.hidden = true
+            playBtn.hidden = true
+            sumLbl.hidden = false
+            addBtn.hidden = false
+            
+            resetLbl()
+            multiple = Int(whatMultipleTxt.text!)!
+            runningSum = 0
+        }
     }
-
-
+    
+    func restartGame() {
+        multiple = 0
+        whatMultipleTxt.text = ""
+        whatMultipleTxt.hidden = false
+        logoImg.hidden = false
+        playBtn.hidden = false
+        sumLbl.hidden = true
+        addBtn.hidden = true
+        resetLbl()
+    }
+    
+    func resetLbl() {
+        sumLbl.text = "Naciśnij Dodaj!"
+    }
+    
+    func isGameOver() -> Bool {
+        if runningSum >= MAX_SUM {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func updateLbl(oldSum: Int, mul: Int, newSum: Int) {
+        sumLbl.text = "\(oldSum) + \(mul) = \(newSum)"
+    }
+    
+    
 }
 
